cd4434a00244267b7099089d8d7b0facd171320b
jay-1104 <jaysohaliya5726@gmail.com>
2026-01-26 16:15:56 +0530

    Add files via upload

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,10 @@ from django.views.generic import CreateView
 from accounts.models import CustomUser
 from django.contrib import messages
 from django.urls import reverse_lazy
+from django.conf import settings
+from django.core.mail import send_mail
+from accounts.models import CustomUser
+
 User=get_user_model()
 
 class Usercreate(CreateView):
@@ -17,6 +21,14 @@ class Usercreate(CreateView):
         user = form.save(commit=False)
         user.set_password(form.cleaned_data['password1'])
         user.save()
+        # send_mail(
+        #     'Task Manager System',
+        #     'Your Registration SuccesFully',
+        #     'monata123123@gmail.com',
+        #     ['jaysohaliya5726@gmail.com'],
+        #     fail_silently=False,
+
+        # )
         messages.success(self.request,"Your Registration SuccessFully ")
         return super().form_valid(form)
     
