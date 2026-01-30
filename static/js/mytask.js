2436f9827216cd2b7a64b9078456148a12116cc5
jay-1104 <jaysohaliya5726@gmail.com>
2026-01-30 21:34:18 +0530

    Add files via upload

--- static/js/mytask.js
+++ static/js/mytask.js
@@ -1,33 +1,17 @@
-function filterTasks(status) {
+function filterTasks(status, btn) {
     const rows = document.querySelectorAll(".task-row");
     const buttons = document.querySelectorAll(".filters button");
 
-    buttons.forEach(btn => btn.classList.remove("active"));
-    // event.target.classList.add("active");
+    buttons.forEach(b => b.classList.remove("active"));
+    btn.classList.add("active");
 
     rows.forEach(row => {
         if (status === "all" || row.dataset.status === status) {
-            row.style.display = "grid";
+            row.style.display = "flex";
         } else {
             row.style.display = "none";
         }
     });
-}
-
-
-/* STATUS TOGGLE (UI ONLY) */
-function toggleStatus(btn) {
-    if (btn.innerText === "Pending") {
-        btn.innerText = "Completed";
-        btn.classList.remove("pending");
-        btn.classList.add("completed");
-        btn.closest(".task-row").dataset.status = "Completed";
-    } else {
-        btn.innerText = "Pending";
-        btn.classList.remove("completed");
-        btn.classList.add("pending");
-        btn.closest(".task-row").dataset.status = "Pending";
-    }
 }
 
 /* SEARCH */
@@ -35,7 +19,7 @@ document.getElementById("searchInput").addEventListener("input", function () {
     const value = this.value.toLowerCase();
     document.querySelectorAll(".task-row").forEach(row => {
         row.style.display = row.dataset.title.includes(value)
-            ? "grid"
+            ? "flex"
             : "none";
     });
 });
