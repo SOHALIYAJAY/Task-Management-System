b4567ea5dae230733d256fc8b7ce6b242b2c5b95
jay-1104 <jaysohaliya5726@gmail.com>
2026-02-04 22:14:33 +0530

    Add files via upload

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,28 +1,11 @@
-const editBtn = document.getElementById("editBtn");
-const saveBtn = document.getElementById("saveBtn");
-const inputs = document.querySelectorAll(".field input");
+function toggleEdit() {
+    const inputs = document.querySelectorAll(
+        '#profileForm input, #profileForm textarea'
+    );
 
-editBtn.onclick = () => {
-    inputs.forEach(i => i.disabled = false);
-    saveBtn.disabled = false;
-};
-
-saveBtn.onclick = () => {
-    document.getElementById("displayName").innerText =
-        document.getElementById("name").value;
-
-    document.getElementById("displayRole").innerText =
-        document.getElementById("role").value;
-
-    inputs.forEach(i => i.disabled = true);
-    saveBtn.disabled = true;
-    alert("Profile Updated!");
-};
-
-document.getElementById("upload").addEventListener("change", e => {
-    const file = e.target.files[0];
-    if (file) {
-        document.getElementById("profilePic").src =
-            URL.createObjectURL(file);
-    }
-});
+    inputs.forEach(input => {
+        input.readOnly = false;
+        input.disabled = false;
+        input.classList.add('editable');
+    });
+}
